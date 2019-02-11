9c13fef835a4b4f7e00dfa918596f03a4818262f
Zachary Ranes <zachary.mt.ranes@gmail.com>
2019-02-11 06:58:58 -0500

    Still need to figure out unittesting

--- my_code.py
+++ my_code.py
@@ -1,5 +1,24 @@
-def problem01():
+def problem_01(list_x, int_y):
     """
-    Write code that returns 2 
+    Write a function that:
+    takes a list x and an int y as input
+    returns a new list that is made of every y elemnet of list x
+    example: list_x = [1,2,3,4,5,6,7,8,9] int_y = 2 return [2,4,6,8] 
     """
-    return 2
+    list_r = []
+    for i in range(0,len(list_x)):
+        if ((i + 1) % int_y) == 1:
+            list_r.append(list_x[i])
+    return list_r
+
+def problem_02():
+    """
+    Write code that:  
+    """
+    pass
+
+def problem_03():
+    """
+    Write code that:  
+    """
+    pass
--- problem_tester.py
+++ problem_tester.py
@@ -1,10 +1,19 @@
 import unittest
+import random
 import my_code
 
-class TestStringMethods(unittest.TestCase):
+class TestMyCode(unittest.TestCase):
 
-    def test_01(self):
-        self.assertAlmostEqual(my_code.problem01(),2)
+    def test(self):
+        for n in range(100):
+            self.r_list = random.sample(range(1,30), random.randrange(2,30))
+            self.r_int = random.randrange(1,len(self.r_list))
+            self.r_list_c = []
+            for i in range(0,len(self.r_list)):
+                if ((i + 1) % self.r_int) == 0:
+                    self.r_list_c .append(self.r_list[i])
+            self.assertAlmostEqual(my_code.problem_01(self.r_list, self.r_int),self.r_list_c, \
+                msg=("???"))
 
 if __name__ == '__main__':
     unittest.main()
